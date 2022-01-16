ce496910d871642aedf20cde03bcd14c56eaa6e5
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-01-16 13:09:39 -0600

    Apply format

--- chat-sockets/ChatClient.java
+++ chat-sockets/ChatClient.java
@@ -5,72 +5,81 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.net.Socket;
+
 public class ChatClient extends JFrame implements ActionListener {
- private static final long serialVersionUID = 1L;
-private String screenName;
+    private static final long serialVersionUID = 1L;
+    private String screenName;
 
- private JTextArea enteredText = new JTextArea(10, 32);
- private JTextField typedText = new JTextField(32);
+    private JTextArea enteredText = new JTextArea(10, 32);
+    private JTextField typedText = new JTextField(32);
 
- private Socket socket;
+    private Socket socket;
 
- private Out out;
- private In in;
- public ChatClient(String screenName, String hostName) {
+    private Out out;
+    private In in;
 
- try {
- socket = new Socket(hostName, 4444);
- out = new Out(socket);
- in = new In(socket);
- }
- catch (Exception ex)
- { ex.printStackTrace(); }
- this.screenName = screenName;
+    public ChatClient(String screenName, String hostName) {
 
- addWindowListener(
- new WindowAdapter() {
- public void windowClosing(WindowEvent e) {
- out.close();
- in.close();
- try { socket.close(); }
- catch (Exception ioe) { ioe.printStackTrace(); }
- }
- }
- );
+        try {
+            socket = new Socket(hostName, 4444);
+            out = new Out(socket);
+            in = new In(socket);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        this.screenName = screenName;
 
- enteredText.setEditable(false);
- enteredText.setBackground(Color.LIGHT_GRAY);
- typedText.addActionListener(this);
- Container content = getContentPane();
- content.add(new JScrollPane(enteredText), BorderLayout.CENTER);
- content.add(typedText, BorderLayout.SOUTH);
+        addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {
+                        out.close();
+                        in.close();
+                        try {
+                            socket.close();
+                        } catch (Exception ioe) {
+                            ioe.printStackTrace();
+                        }
+                    }
+                });
 
- setTitle("Chat Client 1.0: [" + screenName + "]");
- setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- pack();
- typedText.requestFocusInWindow();
- setVisible(true);
- }
- public void actionPerformed(ActionEvent e) {
- out.println("[" + screenName + "]: " + typedText.getText());
- typedText.setText("");
- typedText.requestFocusInWindow();
- }
- public void listen() {
- String s;
- while ((s = in.readLine()) != null) {
- enteredText.insert(s + "\n", enteredText.getText().length());
- enteredText.setCaretPosition(enteredText.getText().length());
- }
- out.close();
- in.close();
- try
- { socket.close(); }
- catch (Exception e) { e.printStackTrace(); }
- System.err.println("Closed client socket");
- }
- public static void main(String[] args) {
- ChatClient client = new ChatClient(args[0], args[1]);
- client.listen();
- }
+        enteredText.setEditable(false);
+        enteredText.setBackground(Color.LIGHT_GRAY);
+        typedText.addActionListener(this);
+        Container content = getContentPane();
+        content.add(new JScrollPane(enteredText), BorderLayout.CENTER);
+        content.add(typedText, BorderLayout.SOUTH);
+
+        setTitle("Chat Client 1.0: [" + screenName + "]");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        typedText.requestFocusInWindow();
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        out.println("[" + screenName + "]: " + typedText.getText());
+        typedText.setText("");
+        typedText.requestFocusInWindow();
+    }
+
+    public void listen() {
+        String s;
+        while ((s = in.readLine()) != null) {
+            enteredText.insert(s + "\n", enteredText.getText().length());
+            enteredText.setCaretPosition(enteredText.getText().length());
+        }
+        out.close();
+        in.close();
+        try {
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.err.println("Closed client socket");
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient(args[0], args[1]);
+        client.listen();
+    }
 }
