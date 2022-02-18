c456c1913d480974f8e6803137da10cf507a5cc6
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-02-18 12:07:10 -0600

    Add serialization and reflection

---
+++ serializationReflection/reflection/DumpMethods.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.*;
+public class DumpMethods {
+    public static void main(String args[])
+    {
+        try {
+            Class c = Class.forName("java.io.ObjectOutput");
+            Method m[] = c.getDeclaredMethods();
+            for (int i = 0; i < m.length; i++)
+                System.out.println(m[i].toString());
+        }
+        catch (Throwable e) {
+          System.out.println("Error");
+            System.err.println(e);
+        }
+    }
+}
